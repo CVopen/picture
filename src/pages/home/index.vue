56e89b04f2c9cc2644a4b7cb8e7f3c74651e49f8
small-navigate <1766730733@qq.com>
2020-06-10 17:36:50 +0800

    本应该是home的分支

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,60 @@
 <template>
-  <view>首页</view>
-</template>
+    <view>
+        <uni-segmented-control
+            :current="current"
+            :values="items.map(v => v.title)"
+            @clickItem="onClickItem"
+            style-type="text"
+            active-color="#d4237a"
+        ></uni-segmented-control>
+        <view class="content">
+            <view v-show="current === 0">
+                <home-category></home-category>
+            </view>
+            <view v-show="current === 1">
+                <home-recommend></home-recommend>
+            </view>
+            <view v-show="current === 2">
+                <home-new></home-new>
+            </view>
+            <view v-show="current === 3">
+                <home-album></home-album>
+            </view>
+        </view>
+    </view>
+</template> 
 <script>
+import homeAlbum from './home-album'
+import homeCategory from './home-category'
+import homeNew from './home-new'
+import homeRecommend from './home-recommend'
+import { uniSegmentedControl } from '@dcloudio/uni-ui'
 export default {
-  
+    components: {
+        homeAlbum,
+        homeCategory,
+        homeNew,
+        homeRecommend,
+        uniSegmentedControl
+    },
+    data() {
+        return {
+            items: [
+                { title: '推荐' },
+                { title: '分类' },
+                { title: '最新' },
+                { title: '专辑' }
+            ],
+            current: 0
+        }
+    },
+    methods: {
+        onClickItem(e) {
+            if (this.current !== e.currentIndex) {
+                this.current = e.currentIndex
+            }
+        }
+    }
 }
 </script>
-<style lang="scss">
-  
-</style>
+<style lang="scss"></style>
